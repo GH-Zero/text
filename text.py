e37b18ecccd048a469f7a9a004c08ba0c6d85279
zero <47428516+GH-Zero@users.noreply.github.com>
2019-10-24 00:11:38 +0800

    Create text.py

---
+++ text.py
@@ -0,0 +1,29 @@
+# text
+如何通过python将excel数据保存文本
+#给system变成/*a*/s./*a*/y./*a*/s./*a*/t./*a*/e./*a*/m/*a*/
+q="system"
+m=list(q)
+i=0
+l=len(m)*2
+while i<=12:
+
+   if i==0:
+       m.insert(i,"/*a*/")
+   elif 0<i<l:
+       m.insert(i,"./*a*/")
+   else:
+        m.insert(i,"/*a*/")
+   i=i+2
+h=''.join(m)
+print(h)
+#给这些字符串变成./*a*/
+q="asdfghjklzxcvbnmqwertuiopojhbvbghjjdkkskiwwuehsbxbuxbxjdjdieiehebehyyebbrkwkwbsbasuduhedjssjejsjnssjwowiwiwi"
+m=list(q)
+i=0
+l=len(m)*2
+while i<=l:
+    m.insert(i,"./*a*/")
+
+    i=i+2
+h=''.join(m)
+print(h)
